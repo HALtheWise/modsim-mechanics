d58e1a0f8ff8e67d3badbb46cd042b3c28b4c718
Eric Miller <eric@legoaces.org>
2015-11-19 11:56:08 -0500

    Included Nathan's changes

---
+++ First-pass trebuchet/flows.m
@@ -0,0 +1,31 @@
+function [res] = flows(~,stocks)
+    %returns the rate of change of our position and velocity
+    
+    %constants
+    m1 = 1;
+    m2 = 1;
+    m3 = 1;
+    l1 = 1;
+    l2 = 1;
+    l3 = 1;
+    g = 1;
+    
+    %things that change   
+    theta1 = stocks(1);
+    theta2 = stocks(2);
+    theta1dot = stocks(3);
+    theta2dot = stocks(4);
+    
+    %rate of change
+    theta1dotdot = ((g * l1 * cos(theta1) * m1) - (l2 (g * cos(theta1) * m2 + sin(theta1 - theta2) * m3 ...
+        * (-g * sin(theta2) + l2 * cos(theta1 - theta2) * theta1dot^2 + l3 * theta2dot^2))) / ...
+        (l1^2 * m1 + l2^2 * (m2 + sin(theta1 - theta2)^2 * m3)));
+    
+    theta2dotdot = ((-l1 * m1 * (g * l2 * cos(theta1) * cos(theta1 - theta2) + ...
+        g * l1 * cos(theta2) - l1 * l2 * sin(theta1 - theta2) * theta1dot^2) + ...
+        l2^2 * sin(theta1 - theta2) * (m2 * (-g * sin(theta1) + l2 * theta1dot^2) + ...
+        m3 * (-g * sin(theta1) + l2 * theta1dot^2 + l3 * cos(theta1 - theta2) * theta2dot^2)))/ ...
+        (l3 * (l1^2 * m1 + l2^2 * (m2 + sin(theta1 - theta2)^2 * m3))));
+    
+    res = [theta1dot; theta2dot; theta1dotdot; theta2dotdot];
+end
