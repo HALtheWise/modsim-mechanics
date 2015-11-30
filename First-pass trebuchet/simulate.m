61e12378551bc0f212da6f5a200364e6256d5650
Eric Miller <eric@legoaces.org>
2015-11-30 11:02:11 -0500

    Adjusted paramenters

--- First-pass trebuchet/simulate.m
+++ First-pass trebuchet/simulate.m
@@ -5,7 +5,7 @@
 
 stocks = [(4*pi/3), 0, 0, 0];
 
-[Times, Stocks] = ode45(@flows, [0, 1], stocks);
+[Times, Stocks] = ode45(@flows, [0, 5], stocks);
 plot(Times, Stocks);
 legend('theta1', 'theta2', 'theta1dot', 'theta2dot')
 
