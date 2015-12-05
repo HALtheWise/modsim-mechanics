ca4681230fb7237c39059df8d5ca3d1ef3092d6d
Eric Miller <eric@legoaces.org>
2015-12-05 16:25:40 -0500

    Did some useful optimization work

--- First-pass trebuchet/X_Optimizer.m
+++ First-pass trebuchet/X_Optimizer.m
@@ -4,25 +4,29 @@ function X_Optimizer()
     totalFOMtime = 0;
 
     %% Simulation
-    function res = testPoint(controls)
+    function [res, params] = testPoint(controls)
         params = parameters();
         
         params.pinAngle = controls(1);
-        params.l1 = controls(2);
-        params.l2 = controls(3);
-        params.l3 = controls(4);
+        %params.l1 = controls(2);
+        params.l2 = controls(2);
+        params.l3 = controls(3);
         
         [Times, Stocks] = simulate(params);
         fom = figuresOfMerit(Times, Stocks, params);
-        res = fom.flightDistance;
+        res = -fom.flightDistance;
     end
 
     %% Guess
     params = parameters();
-    guess = [params.pinAngle, params.l1, params.l2, params.l3];
+    guess = [params.pinAngle, params.l2, params.l3];
 
     %% Optimize
-    fminsearch(@testPoint, guess);
+    optimal = fminsearch(@testPoint, guess)
+    
+    global optimalparams;
+
+    [~, optimalparams] = testPoint(optimal);
     
     %keyboard
 end
