4d9eca271c445708e1faad1a72ebcc7f66230081
Nathan <nathaniel.yee@students.olin.edu>
2015-12-05 16:24:48 -0500

    animation code now uses params

--- First-pass trebuchet/animate.m
+++ First-pass trebuchet/animate.m
@@ -1,12 +1,19 @@
 function res = animate(Times, Stocks, params)
+%%Calculate positions from params
+    p = params;
 
-%% Calculating joint coordinates for animation purposes
+    theta1 = Stocks(:,1);
+    theta2 = Stocks(:,2);
+    
+    P0 =        zeros(45, 2);
+    P1 =        [p.l1*-cos(theta1), p.l1*-sin(theta1)];
+    P2 =        [p.l2*cos(theta1), p.l2*sin(theta1)];
+    P3 = P2 +   [p.l3*cos(theta2), p.l3*sin(theta2)];
+    
 
-
-% It seems more natural for this data to be stored as P1 (containing x and
-% y coords for the first point, P2 (etc.)
-x = [params.l2*cos(Stocks(:,1)), params.l2*cos(Stocks(:,1))+params.l2*cos(Stocks(:,2)), params.l1*-cos(Stocks(:,1))];
-y = [params.l2*sin(Stocks(:,1)), params.l2*sin(Stocks(:,1))+params.l2*sin(Stocks(:,2)), params.l1*-sin(Stocks(:,1))];
+%% Assign joint coordinates
+x = [P1(:,1), P0(:,1), P2(:,1), P3(:,1)];
+y = [P1(:,2), P0(:,2), P2(:,2), P3(:,2)];
 
 %% Convert radians to degrees
 angles = Stocks(:,1:2) * (180/pi);
@@ -22,28 +29,28 @@ hh1(2) = line(Times(1), angles(1,2), 'Marker', '.', 'MarkerSize', 20, ...
 xlabel('time (sec)')
 ylabel('angle (deg)')
 subplot(2,1,2)
-hh2 = plot([0, 0, x(1,1);x(1,3), x(1,1), x(1,2)], [0, 0, y(1,1);y(1,3), y(1,1), y(1,2)], ...
+hh2 = plot([x(1,1), x(1,2), x(1,3);x(1,2), x(1,3), x(1,4)], [y(1,1), y(1,2), y(1,3);y(1,2), y(1,3), y(1,4)], ...
     '.-', 'MarkerSize', 20, 'LineWidth', 2);
 axis equal
-axis([-params.l2-params.l2 params.l2+params.l2 -params.l2-params.l2 params.l2+params.l2])
+axis([-p.l2-p.l2 p.l2+p.l2 -p.l2-p.l2 p.l2+p.l2])
 ht = title(sprintf('Time: %0.2f sec', Times(1)));
 
 %% Get figure size
-pos = get(gcf, 'Position');
-width = pos(3);
-height = pos(4);
+%pos = get(gcf, 'Position');
+%width = pos(3);
+%height = pos(4);
 
 %% Preallocate data (for storing frame data)
-mov = zeros(height, width, 1, length(Times), 'uint8');
+%mov = zeros(height, width, 1, length(Times), 'uint8');
 
 %% Loop through by changing XData and YData
 for id = 1:length(Times)
     % Update graphics data. This is more efficient than recreating plots.
     set(hh1(1), 'XData', Times(id), 'YData', angles(id, 1))
     set(hh1(2), 'XData', Times(id), 'YData', angles(id, 2))
-    set(hh2(1), 'XData', [0, x(id, 3)], 'YData', [0, y(id, 3)])
-    set(hh2(2), 'XData', [0, x(id, 1)], 'YData', [0, y(id, 1)])
-    set(hh2(3), 'XData', [x(id, 1), x(id, 2)], 'YData', [y(id, 1), y(id, 2)])
+    set(hh2(1), 'XData', [x(id, 1), x(id, 2)], 'YData', [y(id, 1), y(id, 2)])
+    set(hh2(2), 'XData', [x(id, 2), x(id, 3)], 'YData', [y(id, 2), y(id, 3)])
+    set(hh2(3), 'XData', [x(id, 3), x(id, 4)], 'YData', [y(id, 3), y(id, 4)])
     set(ht, 'String', sprintf('Time: %0.2f sec', Times(id)))
     
     % Get frame as an image
