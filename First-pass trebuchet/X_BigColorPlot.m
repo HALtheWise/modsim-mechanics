34daafec31e10396d347d9c18406aaf0678b9b66
Eric Miller <eric@legoaces.org>
2015-12-08 16:08:24 -0500

    Changed densification of points in big color plot to use cubic splines.

--- First-pass trebuchet/X_BigColorPlot.m
+++ First-pass trebuchet/X_BigColorPlot.m
@@ -1,19 +1,23 @@
 %% Calculating
 Masses = linspace(1,100,20);
 Angles = linspace(-100,0,500)*pi/180;
-Results = zeros(length(Masses), length(Angles));
+SparseResults = zeros(length(Masses), length(Angles));
 
 for mass = 1:length(Masses)
     params = parameters();
     params.m3 = Masses(mass);
     Throwdists = X_PinAngleSweep(Angles, params);
-    Results(mass, :) = Throwdists;
+    SparseResults(mass, :) = Throwdists;
 end
+
+%% Densify results
+DenseMasses = linspace(min(Masses), max(Masses), 500);
+DenseResults = interp2(Angles, Masses, SparseResults, Angles, DenseMasses', 'cubic');
 
 %% Plotting
 
 params = parameters();
-contour(Angles, Masses, Results, ...
+contour(Angles, DenseMasses, DenseResults, ...
     [params.distanceToPool - params.poolradius, params.distanceToPool + params.poolradius])
 
 %mycmap = [0 0 0;0 1 0;0 0 0];
